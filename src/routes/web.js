355f390fa056806debf0f5ad4ad1766d530df214
Andres Montaniv <montanoa63@gmail.com>
2022-08-30 12:39:13 -0400

    deploy heroku

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,12 @@
 const { Router, response} = require('express');
 const {getHomePage} = require('../controllers/homepageController');
+const {getWebHook, postWebHook} = require('../controllers/chatBotController');
 const router = Router();
 
 let initWebRoutes = (app) => {
     router.get('/', getHomePage);
+    router.get('/webhook', getWebHook);
+    router.post('/webhook', postWebHook);
 
     return app.use('/', router);
 }
