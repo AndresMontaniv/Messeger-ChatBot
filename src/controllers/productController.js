36192401e1ad8efb528f50486699e3d106a9344a
Andres Montaniv <montanoa63@gmail.com>
2022-09-29 14:53:25 -0400

    dfsd

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,20 +4,20 @@ const Product = require("../models/product");
 const createProduct = async (req, res = response) => {
     const { name } = req.body;
     try {
-        const existProduct = await Usuario.findOne({ name });
+        const existProduct = await Product.findOne({ name });
         if (existProduct) {
             return res.status(400).json({
                 ok: false,
                 message: 'Ese nombre ya esta ocupado'
             });
         }
-        const user = new Product(req.body);
+        const product = new Product(req.body);
 
-        await user.save();
+        await product.save();
 
         res.json({
             ok: true,
-            user
+            product
         });
 
     } catch (error) {
