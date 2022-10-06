44670d94559f5b65a36db4ee73e2b6c5bd1d6261
Andres Montaniv <montanoa63@gmail.com>
2022-10-06 17:46:09 -0400

    Sdfsdf

--- src/controllers/visitController.js
+++ src/controllers/visitController.js
@@ -45,10 +45,21 @@ const editVisit = async (facebookId, req) => {
         console.log('visit', currentVisit);
         if (currentVisit) {
             await currentVisit.save();
-            return;
+            return currentVisit;
         }
 
-        return currentVisit;
+        const visit = new Visit({ client: client.id });
+
+        await visit.save();
+        const currentVisita = await Visit.findOneAndUpdate(
+            {
+                client: client,
+                isClosed: false
+            }, req
+        );
+
+        return currentVisita;
+
 
     } catch (error) {
         console.log(error);
