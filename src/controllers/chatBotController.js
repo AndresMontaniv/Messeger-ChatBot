a3d42a6933b0168f88893ee4523122a99b00f802
Andres Montaniv <montanoa63@gmail.com>
2022-10-07 20:41:52 -0400

    nonon

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -310,6 +310,7 @@ async function handleDialogFlowAction(
 
     case "puntuacion.action": //Gracias por tu valoración, nos ayuda a seguir mejorando. ¡Que tenga un buen dia!
       var score = parameters.fields.puntuacion.stringValue;
+      console.log('params==>', parameters.fields);
       console.log('score==>', score);
       await editVisit(sender, { score, isClosed: true });
       handleMessages(messages, sender);
